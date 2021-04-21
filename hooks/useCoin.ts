165dc5108b9180300361cdbfcef29e0cfafa1b8b
Hansol Lee <hansol@nosearch.com>
2021-04-21 17:09:38 +0900

    add react-query

---
+++ hooks/useCoin.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+
+interface Coin {
+  market: string;
+  korean_name: string;
+  english_name: string;
+}
+
+const useCoin = () => {
+  const { data, isFetching } = useQuery("coinList", async () => {
+    const data = await axios.get(
+      "https://api.upbit.com/v1/market/all?isDetails=false"
+    );
+    await new Promise((r) => setTimeout(r, 3000));
+    return data;
+  });
+  return {
+    data: data
+      ? (data.data
+          .filter((row) => row.market.startsWith("KRW"))
+          .slice(0, 20) as Coin[])
+      : [],
+    isFetching,
+  };
+};
+
+export default useCoin;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Button from "antd-mobile/lib/button";
 import Head from "next/head";
-import React from "react";
+import React, { useEffect } from "react";
 import {
   Badge,
   List,
@@ -10,10 +10,14 @@ import {
   WhiteSpace,
   Checkbox,
   Picker,
+  Card,
 } from "antd-mobile";
 import "antd-mobile/dist/antd-mobile.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+import useCoin from "../hooks/useCoin";
 const CheckboxItem = Checkbox.CheckboxItem;
 const AgreeItem = Checkbox.AgreeItem;
+const queryClient = new QueryClient();
 
 const data = [
   { value: 0, label: "Ph.D." },
@@ -34,140 +38,144 @@ const tabs2 = [
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <QueryClientProvider client={queryClient}>
+      <div className="container">
+        <Head>
+          <title>Ant design mobile 테스트</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <div>
-        <Button
-          style={{
-            margin: 20,
-          }}
-          type="primary"
-          onClick={() => {
-            Toast.info("This is a toast tips !!!", 1);
-          }}
-        >
-          Start
-        </Button>
-        <SearchBar placeholder="Search" maxLength={8} cancelText="취소" />
-        <WhiteSpace />
-        <Tabs
-          tabs={tabs}
-          initialPage={0}
-          onChange={(tab, index) => {
-            console.log("onChange", index, tab);
-          }}
-          onTabClick={(tab, index) => {
-            console.log("onTabClick", index, tab);
-          }}
-        >
-          <div
+        <div>
+          <Button
             style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              height: "150px",
-              backgroundColor: "#fff",
+              margin: 20,
+            }}
+            type="primary"
+            onClick={() => {
+              Toast.info("This is a toast tips !!!", 1);
             }}
           >
-            Content of first tab
-          </div>
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              height: "150px",
-              backgroundColor: "#fff",
+            토스트 띄우기
+          </Button>
+          <SearchBar placeholder="Search" maxLength={8} cancelText="취소" />
+          <WhiteSpace />
+          <Tabs
+            tabs={tabs}
+            initialPage={0}
+            onChange={(tab, index) => {
+              console.log("onChange", index, tab);
+            }}
+            onTabClick={(tab, index) => {
+              console.log("onTabClick", index, tab);
             }}
           >
-            Content of second tab
-          </div>
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              height: "150px",
-              backgroundColor: "#fff",
-            }}
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                height: "150px",
+                backgroundColor: "#fff",
+              }}
+            >
+              Content of first tab
+            </div>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                height: "150px",
+                backgroundColor: "#fff",
+              }}
+            >
+              Content of second tab
+            </div>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                height: "150px",
+                backgroundColor: "#fff",
+              }}
+            >
+              Content of third tab
+            </div>
+          </Tabs>
+          <WhiteSpace />
+          <Tabs
+            tabs={tabs2}
+            initialPage={0}
+            tabBarPosition="bottom"
+            renderTab={(tab) => <span>{tab.title}</span>}
           >
-            Content of third tab
-          </div>
-        </Tabs>
-        <WhiteSpace />
-        <Tabs
-          tabs={tabs2}
-          initialPage={0}
-          tabBarPosition="bottom"
-          renderTab={(tab) => <span>{tab.title}</span>}
-        >
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              height: "150px",
-              backgroundColor: "#fff",
-            }}
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                height: "150px",
+                backgroundColor: "#fff",
+              }}
+            >
+              Content of first tab
+            </div>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                height: "150px",
+                backgroundColor: "#fff",
+              }}
+            >
+              Content of second tab
+            </div>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                height: "150px",
+                backgroundColor: "#fff",
+              }}
+            >
+              Content of third tab
+            </div>
+          </Tabs>
+          <WhiteSpace />
+          <List renderHeader={() => "CheckboxItem demo"}>
+            {data.map((i) => (
+              <CheckboxItem key={i.value}>{i.label}</CheckboxItem>
+            ))}
+            <CheckboxItem
+              key="disabled"
+              data-seed="logId"
+              disabled
+              defaultChecked
+              multipleLine
+            >
+              Undergraduate<List.Item.Brief>Auxiliary text</List.Item.Brief>
+            </CheckboxItem>
+          </List>
+          <WhiteSpace />
+          <Picker
+            data={seasons}
+            title="분기 선택"
+            cascade={false}
+            extra="2021년 봄"
+            okText="확인"
+            dismissText="닫기"
+            // value={this.state.sValue}
+            // onChange={(v) => this.setState({ sValue: v })}
+            // onOk={(v) => this.setState({ sValue: v })}
           >
-            Content of first tab
-          </div>
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              height: "150px",
-              backgroundColor: "#fff",
-            }}
-          >
-            Content of second tab
-          </div>
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              height: "150px",
-              backgroundColor: "#fff",
-            }}
-          >
-            Content of third tab
-          </div>
-        </Tabs>
-        <WhiteSpace />
-        <List renderHeader={() => "CheckboxItem demo"}>
-          {data.map((i) => (
-            <CheckboxItem key={i.value}>{i.label}</CheckboxItem>
-          ))}
-          <CheckboxItem
-            key="disabled"
-            data-seed="logId"
-            disabled
-            defaultChecked
-            multipleLine
-          >
-            Undergraduate<List.Item.Brief>Auxiliary text</List.Item.Brief>
-          </CheckboxItem>
-        </List>
-        <WhiteSpace />
-        <Picker
-          data={seasons}
-          title="분기 선택"
-          cascade={false}
-          extra="2021년 봄"
-          okText="확인"
-          dismissText="닫기"
-          // value={this.state.sValue}
-          // onChange={(v) => this.setState({ sValue: v })}
-          // onOk={(v) => this.setState({ sValue: v })}
-        >
-          <List.Item arrow="horizontal">분기 선택</List.Item>
-        </Picker>
+            <List.Item arrow="horizontal">분기 선택</List.Item>
+          </Picker>
+          <WhiteSpace />
+          <CoinComponent />
+        </div>
       </div>
       <style jsx global>{`
         @import url(//fonts.googleapis.com/earlyaccess/nanumgothic.css);
@@ -185,9 +193,38 @@ export default function Home() {
           margin-bottom: 50px;
         }
       `}</style>
-    </div>
+    </QueryClientProvider>
   );
 }
+
+const CoinComponent = () => {
+  const { data, isFetching } = useCoin();
+
+  useEffect(() => {
+    if (isFetching) {
+      Toast.loading("Loading..", 3000);
+    } else {
+      Toast.hide();
+    }
+  }, [isFetching]);
+  return (
+    <div>
+      {data.map((coinObj) => {
+        return (
+          <Card key={coinObj.english_name} style={{ margin: 10 }}>
+            <Card.Header
+              title={coinObj.korean_name}
+              extra={<span>{coinObj.market}</span>}
+            />
+            <Card.Body>
+              <div>{coinObj.english_name}</div>
+            </Card.Body>
+          </Card>
+        );
+      })}
+    </div>
+  );
+};
 
 const seasons = [
   [
