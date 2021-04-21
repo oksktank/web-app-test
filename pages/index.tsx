588d9e6e92c357fd37a2cfaaa46122168f46006d
Hansol Lee <hansol@nosearch.com>
2021-04-21 16:07:37 +0900

    add some components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,25 @@
 import Button from "antd-mobile/lib/button";
 import Head from "next/head";
 import React from "react";
-import { Badge, Tabs, Toast, WhiteSpace } from "antd-mobile";
+import {
+  Badge,
+  List,
+  SearchBar,
+  Tabs,
+  Toast,
+  WhiteSpace,
+  Checkbox,
+  Picker,
+} from "antd-mobile";
 import "antd-mobile/dist/antd-mobile.css";
+const CheckboxItem = Checkbox.CheckboxItem;
+const AgreeItem = Checkbox.AgreeItem;
 
+const data = [
+  { value: 0, label: "Ph.D." },
+  { value: 1, label: "Bachelor" },
+  { value: 2, label: "College diploma" },
+];
 const tabs = [
   { title: <Badge text={"3"}>첫번째 탭</Badge> },
   { title: <Badge text={"20"}>두번째 탭</Badge> },
@@ -26,15 +42,21 @@ export default function Home() {
 
       <div>
         <Button
+          style={{
+            margin: 20,
+          }}
+          type="primary"
           onClick={() => {
             Toast.info("This is a toast tips !!!", 1);
           }}
         >
           Start
         </Button>
+        <SearchBar placeholder="Search" maxLength={8} cancelText="취소" />
+        <WhiteSpace />
         <Tabs
           tabs={tabs}
-          initialPage={1}
+          initialPage={0}
           onChange={(tab, index) => {
             console.log("onChange", index, tab);
           }}
@@ -79,7 +101,7 @@ export default function Home() {
         <WhiteSpace />
         <Tabs
           tabs={tabs2}
-          initialPage={1}
+          initialPage={0}
           tabBarPosition="bottom"
           renderTab={(tab) => <span>{tab.title}</span>}
         >
@@ -118,7 +140,58 @@ export default function Home() {
           </div>
         </Tabs>
         <WhiteSpace />
+        <List renderHeader={() => "CheckboxItem demo"}>
+          {data.map((i) => (
+            <CheckboxItem key={i.value}>{i.label}</CheckboxItem>
+          ))}
+          <CheckboxItem
+            key="disabled"
+            data-seed="logId"
+            disabled
+            defaultChecked
+            multipleLine
+          >
+            Undergraduate<List.Item.Brief>Auxiliary text</List.Item.Brief>
+          </CheckboxItem>
+        </List>
+        <WhiteSpace />
+        <Picker
+          data={seasons}
+          title="분기 선택"
+          cascade={false}
+          extra="2021년 봄"
+          okText="확인"
+          dismissText="닫기"
+          // value={this.state.sValue}
+          // onChange={(v) => this.setState({ sValue: v })}
+          // onOk={(v) => this.setState({ sValue: v })}
+        >
+          <List.Item arrow="horizontal">분기 선택</List.Item>
+        </Picker>
       </div>
     </div>
   );
 }
+
+const seasons = [
+  [
+    {
+      label: "2020",
+      value: "2020",
+    },
+    {
+      label: "2021",
+      value: "2021",
+    },
+  ],
+  [
+    {
+      label: "봄",
+      value: "봄",
+    },
+    {
+      label: "여름",
+      value: "여름",
+    },
+  ],
+];
